047ba5c542bb84747ccafd42c6655cede78fd13f
Warder <44697180+PiconHub-Warder@users.noreply.github.com>
2026-09-12 17:28:55 +0200

    Load asynchronous update UI patch

--- plugin-update/files/__init__.py
+++ plugin-update/files/__init__.py
@@ -38,3 +38,6 @@ except Exception as e: print('[PiconHub] auto restart patch load error:', e)
 try:
     from . import update_menu  # noqa: F401
 except Exception as e: print('[PiconHub] update menu load error:', e)
+try:
+    from . import ui_async_patch  # noqa: F401
+except Exception as e: print('[PiconHub] UI/async patch load error:', e)
